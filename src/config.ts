c722977229714e4116e740e209f3294b8f45228a
meddle2 <meddle777@gmail.com>
2025-11-10 18:20:47 +0100

    init

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+
+export const THEME_COLOR = '#F6F4EE';
+export const NAV_ITEMS = [
+    {
+        name: 'gabinet',
+        slug: '/',
+    },
+    {
+        name: 'o mnie', 
+        slug: '/o-mnie'
+    },
+    {
+        name: 'o medycynie chińskiej', 
+        slug: '/o-medycynie-chinskiej'
+    },
+] as const
+
+// main page content stuff
+export const TITLE_OF_THE_THING = "Gabinet Tradycyjnej i Klasycznej Medycyny Chińskiej"
+export const MY_FULL_NAME = 'Alicja Czekała-Sokołowska'
+export const PHONE = '784 368 207'
+export const SESSION_PRICE = 150
+export const MY_LOCATION = "gabinet znajduje się w Dębienku koło Stęszewa"
