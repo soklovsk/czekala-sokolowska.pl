7a6c38bd1dbb923bea27ecfdbe0c130e0a542524
meddle2 <meddle777@gmail.com>
2026-03-27 16:06:15 +0100

    feat: change config

--- src/config.ts
+++ src/config.ts
@@ -20,6 +20,6 @@ export const TITLE_OF_THE_THING = "Gabinet Tradycyjnej i Klasycznej Medycyny Chi
 export const MY_FULL_NAME = 'Alicja Czekała-Sokołowska'
 export const PHONE = '663 301 364'
 export const FIRST_SESSION_PRICE = 200
-export const NEXT_SESSION_PRICE = 150
+export const NEXT_SESSION_PRICE = 170
 export const MY_LOCATION = "Gabinet znajduje się w Dębienku koło Stęszewa."
 export const BOOK_INFO = "W celu umówienia wizyty proszę o kontakt telefoniczny lub SMS."
